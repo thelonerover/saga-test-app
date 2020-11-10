4383f09a85477c6941bf57eb650438e1cb436e3a
Vladimir Komarov <34229011+thelonerover@users.noreply.github.com>
2020-11-10 18:30:40 +0300

    ui fixes

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styles from './Loader.module.scss';
 
 function Loader() {
   const loading = useSelector(state => state.ticker.loading);
 
   return (
     <div>
-      {loading && <span>Loading</span>}
+      {loading && <span className={styles.loader}>Loading</span>}
     </div>
   );
 }
